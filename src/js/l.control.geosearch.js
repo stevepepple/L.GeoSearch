1484a17170cf500ec2bb947a84b71b11f4854d71
Add Nokia Here service and dependencies <spepple@spepple-MacBook-Pro.local>
2015-02-24 22:06:52 -0800

    Tweak the Zoom level

--- src/js/l.control.geosearch.js
+++ src/js/l.control.geosearch.js
@@ -20,10 +20,10 @@ L.Control.GeoSearch = L.Control.extend({
 
     _config: {
         country: '',
-        searchLabel: 'search for address ...',
+        searchLabel: 'Search for address ...',
         notFoundMessage: 'Sorry, that address could not be found.',
         messageHideDelay: 3000,
-        zoomLevel: 18
+        zoomLevel: 16
     },
 
     initialize: function (options) {
